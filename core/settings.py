7e99c47551f3565f642e99859232160c7b66f3ab
Thiago <thiagotavares2002@gmail.com>
2024-06-05 19:53:50 -0300

    style: UploadingHomePageInAPI

--- core/settings.py
+++ core/settings.py
@@ -4,6 +4,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOGIN_URL = '/auth/login/'  # Defina isso para a URL de login correta no seu projeto
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -108,6 +109,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'templates',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- project/urls.py
+++ project/urls.py
@@ -1,9 +1,13 @@
 #urls.py/project
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('cadastro/', views.cadastro_usuario_view, name='cadastro'),
+    path('home/', views.home_view, name='home'),
+    path('', views.index_view, name='index'),
 ]
