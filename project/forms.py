53bf581c412a0c95bf32706327869d1508103dc9
Thiago <thiagotavares2002@gmail.com>
2024-06-12 22:09:44 -0300

    docs: AddingComentaryBackendCode

--- project/forms.py
+++ project/forms.py
@@ -1,7 +1,20 @@
-from django import forms
-from .models import Product
+from django import forms  # Importa o módulo de formulários do Django
+from .models import Product  # Importa o modelo Product do diretório atual
 
 class ProductForm(forms.ModelForm):
+    """
+    Formulário para criação e edição de produtos.
+
+    Campos:
+    - name: Nome do produto (CharField)
+    - quantity: Quantidade disponível do produto (IntegerField)
+    - price: Preço do produto (DecimalField)
+
+    Meta:
+    - model: Define o modelo vinculado ao formulário (Product)
+    - fields: Define os campos do modelo a serem incluídos no formulário ('name', 'quantity', 'price')
+    """
+
     class Meta:
-        model = Product
-        fields = ['name', 'quantity', 'price']
+        model = Product  # Define o modelo vinculado ao formulário
+        fields = ['name', 'quantity', 'price']  # Define os campos do modelo a serem incluídos no formulário
--- project/views.py
+++ project/views.py
@@ -1,87 +1,150 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from project.forms import ProductForm
-from .models import Product
-from django.http import JsonResponse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render, redirect  # Funções para renderizar templates e redirecionar URLs
+from django.contrib.auth import authenticate, login, logout  # Funções para autenticação de usuário
+from django.contrib.auth.decorators import login_required  # Decorador para exigir autenticação
+from django.contrib.auth.forms import UserCreationForm  # Formulário pré-definido para criação de usuário
+from django.urls import reverse_lazy  # Função para resolução reversa de URLs
+from project.forms import ProductForm  # Formulário personalizado para produtos
+from .models import Product  # Importa o modelo Product do diretório atual
+from django.http import JsonResponse  # Respostas HTTP em formato JSON
+from django.shortcuts import get_object_or_404  # Função para obter um objeto do banco de dados ou 404 (não encontrado)
 
 
 def login_view(request):
+    """
+    View para autenticar usuários no sistema.
+
+    Recebe requisições POST com dados de login e autentica o usuário se válido.
+    Redireciona para a página principal ('home') se autenticado ou exibe mensagem de erro.
+
+    Template utilizado: 'login.html'.
+    """
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('home')  # Redirecione para a página principal ou para onde você quiser
+            return redirect('home')  # Redireciona para a página principal após login
         else:
-            return render(request, 'login.html', {'error': 'Usuário ou senha incorretos.'})  # Altere aqui para 'login.html'
+            return render(request, 'login.html', {'error': 'Usuário ou senha incorretos.'})
     else:
-        return render(request, 'login.html')  # Altere aqui para 'login.html'
+        return render(request, 'login.html')
+
 
 @login_required
 def logout_view(request):
+    """
+    View para deslogar usuários do sistema.
+
+    Requer autenticação prévia (decorador @login_required).
+    Redireciona para a página de login ('login') após o logout.
+    """
     logout(request)
     return redirect('login')
 
+
 @login_required
 def home_view(request):
+    """
+    View da página inicial do sistema.
+
+    Requer autenticação prévia (decorador @login_required).
+    Renderiza o template 'home.html'.
+    """
     return render(request, 'home.html')
 
 
 def cadastro_usuario_view(request):
+    """
+    View para cadastrar novos usuários.
+
+    Recebe requisições POST com dados do formulário de criação de usuário.
+    Salva o usuário se o formulário for válido e redireciona para a página de login ('login').
+    Renderiza o template 'formUsuario.html' para entrada de dados.
+
+    Template utilizado: 'formUsuario.html'.
+    """
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            print("Formulário de cadastro válido.")  # Adicione esta instrução de depuração
+            print("Formulário de cadastro válido.")
             form.save()
-            return redirect(reverse_lazy('login'))  # Redireciona para a página de login após o cadastro bem-sucedido
+            return redirect(reverse_lazy('login'))  # Redireciona para o login após cadastro
         else:
-            print("Formulário de cadastro inválido:", form.errors)  # Adicione esta instrução de depuração para imprimir quaisquer erros de validação do formulário
+            print("Formulário de cadastro inválido:", form.errors)
     else:
         form = UserCreationForm()
     return render(request, 'formUsuario.html', {'form': form})
 
+
 def index_view(request):
+    """
+    View para a página inicial ('index').
+
+    Redireciona para 'home' se o usuário estiver autenticado, senão redireciona para 'login'.
+    """
     if request.user.is_authenticated:
         return redirect('home')
     else:
         return redirect('login')
 
-def home(request):
-    return render(request, 'home.html')
 
+@login_required
 def add_product(request):
+    """
+    View para adicionar um novo produto.
+
+    Recebe requisições POST com dados do produto e salva no banco de dados.
+    Redireciona para 'home' após adicionar o produto.
+    Renderiza o template 'add_product.html' para entrada de dados.
+
+    Template utilizado: 'add_product.html'.
+    """
     if request.method == 'POST':
         name = request.POST['name']
         quantity = request.POST['quantity']
         price = request.POST['price']
         Product.objects.create(name=name, quantity=quantity, price=price)
-        return redirect('home')  # Redireciona para a página inicial após adicionar o produto
+        return redirect('home')
     return render(request, 'add_product.html')
 
-def buy_product(request, product_id):
-    # Obtenha o produto do banco de dados
-    product = get_object_or_404(Product, pk=product_id)
 
-    # Verifique se há quantidade disponível para compra
+def buy_product(request, product_id):
+    """
+    View para comprar um produto.
+
+    Recebe o ID do produto como parâmetro na URL.
+    Verifica se há estoque disponível para o produto e decrementa a quantidade se possível.
+    Retorna uma resposta JSON com mensagem de sucesso ou erro.
+
+    Respostas:
+    - {'message': 'Produto comprado com sucesso!'}: Se a compra foi bem-sucedida.
+    - {'error': 'Produto esgotado.'}: Se não há estoque disponível para o produto.
+
+    Requer autenticação para acessar esta função.
+    """
+    product = get_object_or_404(Product, pk=product_id)
     if product.quantity > 0:
-        # Decrementa a quantidade disponível
         product.quantity -= 1
         product.save()
-
-        # Retorne uma resposta de sucesso
         return JsonResponse({'message': 'Produto comprado com sucesso!'})
-
     else:
-        # Retorne uma resposta de erro se não houver quantidade disponível
         return JsonResponse({'error': 'Produto esgotado.'}, status=400)
+
 
 @login_required
 def edit_product(request, pk):
+    """
+    View para editar um produto existente.
+
+    Recebe requisições POST com dados atualizados do produto.
+    Salva as alterações se o formulário for válido e redireciona para 'product-list'.
+    Renderiza o template 'edit_product.html' para edição de dados.
+
+    Requer autenticação prévia (decorador @login_required).
+
+    Template utilizado: 'edit_product.html'.
+    """
     product = get_object_or_404(Product, pk=pk)
     if request.method == 'POST':
         form = ProductForm(request.POST, instance=product)
@@ -92,15 +155,38 @@ def edit_product(request, pk):
         form = ProductForm(instance=product)
     return render(request, 'edit_product.html', {'form': form})
 
+
 @login_required
 def delete_product(request, pk):
+    """
+    View para deletar um produto existente.
+
+    Recebe requisições POST para confirmar a exclusão do produto.
+    Deleta o produto do banco de dados e redireciona para 'product-list'.
+    Renderiza o template 'confirm_delete.html' para confirmação de exclusão.
+
+    Requer autenticação prévia (decorador @login_required).
+
+    Template utilizado: 'confirm_delete.html'.
+    """
     product = get_object_or_404(Product, pk=pk)
     if request.method == 'POST':
         product.delete()
         return redirect('product-list')
     return render(request, 'confirm_delete.html', {'product': product})
 
+
 @login_required
 def product_list_view(request):
+    """
+    View para listar todos os produtos cadastrados.
+
+    Obtém todos os produtos do banco de dados e renderiza o template 'card.html' para exibição.
+    Passa a lista de produtos como contexto para o template.
+
+    Requer autenticação prévia (decorador @login_required).
+
+    Template utilizado: 'card.html'.
+    """
     products = Product.objects.all()
     return render(request, 'card.html', {'products': products})
