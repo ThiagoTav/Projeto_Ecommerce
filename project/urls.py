60cdc15dcd2c50fba035f1ed749c3819b01ad671
Thiago <thiagotavares2002@gmail.com>
2024-06-12 21:35:54 -0300

    bugfix:UrlCorrectionToReachTheMethod

--- project/urls.py
+++ project/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     path('home/', views.home_view, name='home'),
     path('', views.index_view, name='index'),
     path('add-product/', add_product, name='add-product'),
-    
+    path('products/', product_list_view, name='product-list'),  # URL para listar produtos
+    path('edit-product/<int:pk>/', edit_product, name='edit-product'),
+    path('delete-product/<int:pk>/', delete_product, name='delete-product'),
 ]
